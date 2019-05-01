21768b6d9b06cbfe2f1cef8fce94d49d776437fd
Rohit Kumar <46925145+rohit-sahu@users.noreply.github.com>
2019-05-01 11:34:48 +0530

    Add files via upload

--- spring-boot-rest/src/main/java/com/rohit/springbootrest/SpringBootRestApplication.java
+++ spring-boot-rest/src/main/java/com/rohit/springbootrest/SpringBootRestApplication.java
@@ -20,46 +20,46 @@ public class SpringBootRestApplication {
 
 	@Value("${server.port}")
 	private int serverPortHttps;
-	
+
 	@Bean
 	public ServletWebServerFactory servletContainer() {
-	    TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-	        @Override
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+			@Override
 			protected void customizeConnector(Connector connector) {
-	        	int maxSize = 50000000;
-                super.customizeConnector(connector);
-                connector.setMaxPostSize(maxSize);
-                connector.setMaxSavePostSize(maxSize);
-                if (connector.getProtocolHandler() instanceof AbstractHttp11Protocol) {
+				int maxSize = 50000000;
+				super.customizeConnector(connector);
+				connector.setMaxPostSize(maxSize);
+				connector.setMaxSavePostSize(maxSize);
+				if (connector.getProtocolHandler() instanceof AbstractHttp11Protocol) {
 
-                    ((AbstractHttp11Protocol <?>) connector.getProtocolHandler()).setMaxSwallowSize(maxSize);
-                    logger.info("Set MaxSwallowSize "+ maxSize);
-                }
+					((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(maxSize);
+					logger.info("Set MaxSwallowSize " + maxSize);
+				}
 			}
 
 			@Override
-	        protected void postProcessContext(Context context) {
-	            SecurityConstraint securityConstraint = new SecurityConstraint();
-	            securityConstraint.setUserConstraint("CONFIDENTIAL");
-	            SecurityCollection collection = new SecurityCollection();
-	            collection.addPattern("/*");
-	            securityConstraint.addCollection(collection);
-	            context.addConstraint(securityConstraint);
-	        }
-	    };
-	    tomcat.addAdditionalTomcatConnectors(redirectConnector());
-	    return tomcat;
+			protected void postProcessContext(Context context) {
+				SecurityConstraint securityConstraint = new SecurityConstraint();
+				securityConstraint.setUserConstraint("CONFIDENTIAL");
+				SecurityCollection collection = new SecurityCollection();
+				collection.addPattern("/*");
+				securityConstraint.addCollection(collection);
+				context.addConstraint(securityConstraint);
+			}
+		};
+		tomcat.addAdditionalTomcatConnectors(redirectConnector());
+		return tomcat;
 	}
 
 	private Connector redirectConnector() {
-	    Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-	    connector.setScheme("http");
-	    connector.setPort(serverPortHttp);
-	    connector.setSecure(false);
-	    connector.setRedirectPort(serverPortHttps);
-	    return connector;
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setScheme("http");
+		connector.setPort(serverPortHttp);
+		connector.setSecure(false);
+		connector.setRedirectPort(serverPortHttps);
+		return connector;
 	}
-	
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestApplication.class, args);
 	}
